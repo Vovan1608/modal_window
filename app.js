6f044c6d12908c819f50e62d97b952bae3b8183d
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-14 15:37:00 +0300

    added addEventListener

--- app.js
+++ app.js
@@ -2,4 +2,8 @@
 
 const modalBtn = document.querySelector(".modal-btn");
 const closeBtn = document.querySelector(".close-btn");
+const overlay = document.querySelector(".modal-overlay");
 
+modalBtn.addEventListener("click", () => {
+	overlay.classList.add(".open-modal");
+});
